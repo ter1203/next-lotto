6b1c67dceadb5ff893d5c0d188246a49781624a6
sergio1221 <sergio.pinilos.dev@gmail.com>
2021-05-24 15:17:49 +0300

    deposit/withdraw ui done. api not work

--- service/client/user.js
+++ service/client/user.js
@@ -9,3 +9,10 @@ export const signup = (firstName, lastName, email, phone, password, affilliateID
 
 export const getBalance = (memberID) => get('/api/user/balance', { memberID });
 
+export const prepareConfirmDeposit = (memberID, amount, ticker) => post('/api/user/deposit', {
+  memberID, amount, ticker
+});
+
+export const requestWithdraw = (amount, ticker, address) => post('/api/user/withdraw', {
+  amount, ticker, address
+});
