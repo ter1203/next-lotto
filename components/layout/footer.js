09b18de61db5b8f6997628571bb6a360f1df14b0
sergio1221 <sergio.pinilos.dev@gmail.com>
2021-06-07 03:18:23 +0300

    supporting email

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -8,7 +8,7 @@ export default function Footer() {
 			<div id="contact-us-modal" className="modal">
 				<div className="right close-modal"><span className="fa fa-close" rel="modal:close"></span></div>
 				<div className="modal-header">CONTACT US</div>
-				<div className="modal-body">If you want to contact us directly, you can email us at<br /><a href="mailto:support-lottery@bitcoinlotterys.com">support-lottery@bitcoinlotterys.com</a></div>
+				<div className="modal-body">If you want to contact us directly, you can email us at<br /><a href="mailto:support@bitcoinlotterys.com">support@bitcoinlotterys.com</a></div>
 			</div>
 
 			<div id="gradient-bg" className="pt-4 pb-2">
