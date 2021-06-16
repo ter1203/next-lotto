89f40cc6609eeeac795b7db04ab8c9dfc268a0a6
sergio1221 <sergio.pinilos.dev@gmail.com>
2021-06-16 16:48:34 +0300

    some styling

--- pages/help/affiliate.js
+++ pages/help/affiliate.js
@@ -15,19 +15,23 @@ const Affiliate = () => {
 									<h1>Why Join <span className='text-primary'>Bitcoin</span><span className='text-secondary'>Lotterys.com</span> Affiliate Program?</h1>
 								</div>
 								<div className="main-content affiliate">
-									<h1 className="item-title">Net Revenue Share up to 50%</h1>
-									<p>Our affiliates enjoy high percentages and secure income.</p>
-									<h1 className="item-title">No Negative Carryover</h1>
-									<p>At the start of each month any negative balance is reset to zero.</p>
-									<h1 className="item-title">The Fastest Monthly Payouts</h1>
-									<p>We pay on the 1st business day of every month!</p>
-									<h1 className="item-title">Lifetime Earnings</h1>
-									<p>Your referred players bring you lifetime earnings!</p>
-									<h1 className="item-title" id="contact-us">Dedicated Affiliate Management Team</h1>
-									<p>An experienced team ready to help you at any time.</p>
-									<h1 className="item-title" id="contact-us">Unique and Trusted Brand</h1>
-									<p>Unique to <span className='text-primary'>bitcoin</span><span className='text-secondary'>lotterys.com</span> play official lotteries and BTC games your players can’t find elsewhere!</p>
-									&nbsp;&nbsp;<a href="mailto:support@bitcoinlotterys.com" className='playNowBtn'>Join Now</a>
+									<div className='content'>
+										<h1 className="item-title">Net Revenue Share up to 50%</h1>
+										<p>Our affiliates enjoy high percentages and secure income.</p>
+										<h1 className="item-title">No Negative Carryover</h1>
+										<p>At the start of each month any negative balance is reset to zero.</p>
+										<h1 className="item-title">The Fastest Monthly Payouts</h1>
+										<p>We pay on the 1st business day of every month!</p>
+										<h1 className="item-title">Lifetime Earnings</h1>
+										<p>Your referred players bring you lifetime earnings!</p>
+										<h1 className="item-title" id="contact-us">Dedicated Affiliate Management Team</h1>
+										<p>An experienced team ready to help you at any time.</p>
+										<h1 className="item-title" id="contact-us">Unique and Trusted Brand</h1>
+										<p>Unique to <span className='text-primary'>bitcoin</span><span className='text-secondary'>lotterys.com</span> play official lotteries and BTC games your players can’t find elsewhere!</p>
+									</div>
+									<div className='action'>
+										<a href="mailto:support@bitcoinlotterys.com" className='playNowBtn'>Join Now</a>
+									</div>
 								</div>
 							</div>
 						</div>
