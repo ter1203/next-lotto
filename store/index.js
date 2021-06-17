f23313ebc83f13dab21c20e7fecc0c0e26dd31ca
sergio1221 <sergio.pinilos.dev@gmail.com>
2021-06-17 11:16:32 +0300

    store expire time: 30mins

--- store/index.js
+++ store/index.js
@@ -7,8 +7,14 @@ const storeItem = 'lottery-bitcoin';
 const loadState = () => {
   try {
     const serializedState = localStorage.getItem(storeItem);
-    if (serializedState === null) {
+    if (!serializedState || !serializedState.modified_at) {
       return {};
+    }
+
+    // expired data? check if it is 30 mins old
+    const silence = (new Date().getTime() - serializedState.modified_at) / 1000;
+    if (silence > 1800) {    // 30 mins
+      return {}
     }
 
     return JSON.parse(serializedState);
@@ -19,6 +25,7 @@ const loadState = () => {
 
 const saveState = (state) => {
   try {
+    state.modified_at = new Date().getTime();
     const serializedState = JSON.stringify(state);
     localStorage.setItem(storeItem, serializedState);
   } catch (error) {
@@ -26,13 +33,13 @@ const saveState = (state) => {
   }
 };
 
-// const peristedState = loadState();
+const peristedState = loadState();
 export default function configureStore() {
   const storeEnhancers = compose;
   saveState({});
   const store = createStore(
     rootReducer,
-    {},
+    peristedState,
     storeEnhancers(applyMiddleware(thunk))
   );
 
